154c124f006c441cb47d7f71170a1a0d9ba1f151
d_flego <danielflego@gmail.com>
2021-06-10 10:26:18 +0930

    Added Index File

---
+++ app/index.php
@@ -0,0 +1,47 @@
+<?php
+  require "header.php";
+?>
+
+  <main>
+
+    <div class="hero">
+      <!--<div class="hero-container">-->
+
+        <div class="info">
+          <section class="section-info">
+          
+            <p class="login-status">You are logged out!</P>
+            <p class="login-status">You are logged in!</p>
+
+          </section>     
+        </div>
+
+        <div class="hero-content">
+
+          <h1 id="studio">Fox Spirit Pilates</h2>
+          <h2 id="addresses">Port Adelaide, Wynn Vale<br>
+              Visit You
+          </h2>
+
+        </div>
+      <!--</div>-->
+    </div>
+
+  </main>
+
+<?php 
+  require "footer.php";
+?>
+
+
+
+
+
+
+<!--
+  <div class="banner">
+
+    <img class="banner__img" src="../public/img/ocean1.jpg" alt="foxspirit__banner">
+
+  </div>
+-->
