f6ae95f9c2b2b6907d46e1886dc6ab0630c8fef7
d_flego <danielflego@gmail.com>
2021-06-10 10:51:56 +0930

    Added Footer File

---
+++ app/footer.php
@@ -0,0 +1,14 @@
+  <footer>
+  </footer>
+
+    <script src="../public/js/arrays.js"></script>
+    <script src="../public/js/checks.js"></script>
+    <script src="../public/js/functions.js"></script>
+    <script src="../public/js/main.js"></script>
+    <script src="../public/js/menus.js"></script>
+    <script src="../public/js/onload.js"></script>
+
+    <script src="../public/js/dropdown.js"></script>
+
+  </body>
+</hmtl>
